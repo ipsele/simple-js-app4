73b132e027c42a3a14efa91b2d688e033f8cc9c4
ipsele <123554511+ipsele@users.noreply.github.com>
2023-02-04 21:09:56 +0100

    Additional List items

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+let pokemonList = [
+    {name: 'Charmander', 
+    height: 4, 
+    type: 'fire'},
+    {name: 'Nidorina', 
+    height: 3, 
+    type: 'poison'},
+    {name: 'Dewgong', 
+    height: 7, 
+    type:['water','ice']},
+    {name: 'Minun', 
+    height: 2, 
+    type: 'electric'},
+    {name: 'Jigglypuff', 
+    height: 2, 
+    type:['fairy','normal']}
+]
+
