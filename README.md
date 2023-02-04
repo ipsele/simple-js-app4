7c36b12c2097ed882146df85286f8aa06ae90f43
ipsele <123554511+ipsele@users.noreply.github.com>
2023-02-04 23:36:20 +0100

    Comments on approach

---
+++ README.md
@@ -0,0 +1,4 @@
+Definition of new variable pokemonList and responsive values
+Creation of for loop to display selected values
+Implementation of condition and message to be displayed
+List and message are displayed correctly in Chrome & Safari
--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,17 @@ let pokemonList = [
     {name: 'Jigglypuff', 
     height: 2, 
     type:['fairy','normal']}
-]
+];
+
+
+for (let i = 0; i < pokemonList.length; i++) {        
+    if (pokemonList[i].height >= 5) {
+      document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm) - Wow, that\'s a big pokemon! \;' + '<br>');
+    } else if (pokemonList[i].height < 5) {
+      document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm)  \;' + '<br>')
+    } 
+  }
+  
+
+ 
 
